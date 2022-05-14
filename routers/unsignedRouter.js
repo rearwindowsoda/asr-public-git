a02abcd14829ba71cce16a98e98ee568177704bf
MateuszOwczarek <mat.owc@o2.pl>
2022-05-14 04:15:48 +0200

    addded function that clears cookies in unsigned router

--- routers/unsignedRouter.js
+++ routers/unsignedRouter.js
@@ -2,6 +2,7 @@ const express = require('express');
 const unsignedRouter = express.Router();
 
 unsignedRouter.get('/', async (req, res) => {
+	res.clearCookie('jwt');
 	res.render('home/home', {
 		date: new Date(),
 	});
