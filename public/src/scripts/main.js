24018762afa97dba9d668aa15cf9260e81aa8978
MateuszOwczarek <mat.owc@o2.pl>
2022-05-28 00:45:45 +0200

    New app version. Added localstorage validation, changed page layout, bumped version, updated dev notes

--- public/src/scripts/main.js
+++ public/src/scripts/main.js
@@ -1,61 +1,71 @@
-function play(){
-  const audio = new Audio('src/audio/audioclick.mp3');
+function play() {
+  const audio = new Audio("src/audio/audioclick.mp3");
   audio.play();
-};
+}
 
 function showPoints() {
-  const localFetch = localStorage.getItem('all-points') ?? 0
-  const pointsStatus = document.getElementById('points-status');
-  const pointsMultiply = document.getElementById('points-multiply');
+  let localFetch;
+  if (
+    localStorage.getItem("all-points") === undefined ||
+    isNaN(Number(localStorage.getItem("all-points")))
+  ) {
+    localStorage.setItem("all-points", 0);
+    localFetch = "0";
+  } else {
+    localFetch = localStorage.getItem("all-points");
+  }
+  const pointsStatus = document.getElementById("points-status");
+  const pointsMultiply = document.getElementById("points-multiply");
 
-  pointsStatus.innerText = `Liczba pkt. ASR: ${localFetch}`;
-  pointsMultiply.innerText= `Liczba pkt. ${localFetch * 30}`;
+  pointsStatus.innerText = `${localFetch}`;
+  pointsMultiply.innerText = `${localFetch * 30}`;
 }
 
 function addPoints(numberOfPoints) {
-  localStorage.getItem('all-points') ?? localStorage.setItem('all-points', '0');
+  localStorage.getItem("all-points") ?? localStorage.setItem("all-points", "0");
   play();
-  const localFetch = localStorage.getItem('all-points');
+  const localFetch = localStorage.getItem("all-points");
   let userPoints = Number(localFetch) + parseInt(numberOfPoints);
-  localStorage.setItem('all-points', userPoints + '');
+  localStorage.setItem("all-points", userPoints + "");
   showPoints();
   return;
 }
 
-function subtractPoints(numberOfPoints){
-  const localFetch = localStorage.getItem('all-points') ?? 0;
+function subtractPoints(numberOfPoints) {
+  const localFetch = localStorage.getItem("all-points") ?? 0;
   let userPoints = Number(localFetch) - parseInt(numberOfPoints);
-  if (isNaN(userPoints) || userPoints < 0 || parseInt(numberOfPoints) < 0 ){
+  if (isNaN(userPoints) || userPoints < 0 || parseInt(numberOfPoints) < 0) {
     userPoints = 0;
-    alert('Nie możesz wykonać tej operacji.')
+    alert("Nie możesz wykonać tej operacji.");
     return;
   }
 
-  localStorage.setItem('all-points', userPoints + '');
+  localStorage.setItem("all-points", userPoints + "");
   showPoints();
   return;
 }
 
-  const resetButton = document.getElementById('reset-button');
-  resetButton.addEventListener('click', function () {
-    play();
-    const userConfirmation = confirm('Czy na pewno chcesz zresetować dotychczasowe punkty?')
-    if (userConfirmation){
-      alert('Twoje punkty zostaną zresetowane.');
-      localStorage.setItem('all-points', '0')
-      showPoints()}
-      else {
-        alert('Twoje punkty zostały zachowane.')
-      }
-    })
-
-const subtractButton = document.getElementById('subtract-points');
-  const subtractValue = document.querySelector('#subtract-value');
-  subtractButton.addEventListener('click', function() {
+const resetButton = document.getElementById("reset-button");
+resetButton.addEventListener("click", function () {
   play();
- subtractPoints(subtractValue.value);
- subtractValue.value = '';
+  const userConfirmation = confirm(
+    "Czy na pewno chcesz zresetować dotychczasowe punkty?"
+  );
+  if (userConfirmation) {
+    alert("Twoje punkty zostaną zresetowane.");
+    localStorage.setItem("all-points", "0");
+    showPoints();
+  } else {
+    alert("Twoje punkty zostały zachowane.");
+  }
+});
 
-  })
+const subtractButton = document.getElementById("subtract-points");
+const subtractValue = document.querySelector("#subtract-value");
+subtractButton.addEventListener("click", function () {
+  play();
+  subtractPoints(subtractValue.value);
+  subtractValue.value = "";
+});
 
 showPoints();
